ffb97a25c9e3059ebd06591453c95f828ff61c0e
Claude SIMON <epeios-q37@users.noreply.github.com>
2022-10-18 17:02:05 +0200

    Fixing move issue.

--- main.sh
+++ main.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# À renommer en 'main.sh' et à placer dans le dépôt 'GitHub' 'coding-workshops-replit'.
-
 cd ..
 ACTIVITY=Hangman
 REPO=$ACTIVITY-activity-fr
 git clone https://github.com/epeios-q37/$REPO
-mv -r $REPO/* $REPL_SLUG
+mv $REPO/* $REPL_SLUG
 mv $REPO/.replit $REPL_SLUG
 rm $REPL_SLUG/main.sh
